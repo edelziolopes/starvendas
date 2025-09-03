a598eb1a6a6568c7e59e4b7528da5e264496efee
Edelzio Lopes <edelzio@hotmail.com>
2025-09-03 16:01:05 -0300

    2.3

--- Application/views/home/index.php
+++ Application/views/home/index.php
@@ -1,7 +1,20 @@
-<div class="container">
-  <div class="row">
-    <div class="col-8 offset-2 text-center" style="margin-top:150px">
-      <h1>Framework MVC com PHP</h1>
+<div class="container d-flex justify-content-center align-items-center mt-3">
+  <div class="card shadow" style="width: 22rem;">
+    <div class="card-body">
+      <h5 class="card-title mb-4 text-center">Login</h5>
+      <form action="/login" method="post">
+        <div class="mb-3">
+            <label for="username" class="form-label">E-mail:</label>
+          <input type="text" id="username" name="username" class="form-control" required>
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Senha:</label>
+          <input type="password" id="password" name="password" class="form-control" required>
+        </div>
+        <div class="d-grid">
+          <button type="submit" class="btn btn-primary">Entrar</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
