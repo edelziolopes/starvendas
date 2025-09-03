69243a2a83f83f3f728449d134c5995222c09765
Edelzio Lopes <edelzio@hotmail.com>
2025-09-03 15:48:49 -0300

    2.2

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,93 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 18/08/2025 às 18:55
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `ecommerce`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_categorias`
---
-
-CREATE TABLE `tb_categorias` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(40) NOT NULL
+CREATE TABLE tb_carrinhos (
+  id int(11) NOT NULL,
+  id_usuario int(11) NOT NULL,
+  id_endereco int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `tb_categorias`
---
-
-INSERT INTO `tb_categorias` (`id`, `nome`) VALUES
-(1, 'dsjdjds'),
-(2, 'tese');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_produtos`
---
-
-CREATE TABLE `tb_produtos` (
-  `id` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `nome` varchar(40) NOT NULL,
-  `preco` float NOT NULL,
-  `imagem` text NOT NULL,
-  `quantidade` int(11) NOT NULL
+CREATE TABLE tb_categorias (
+  id int(11) NOT NULL,
+  nome varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
+CREATE TABLE tb_compras (
+  id int(11) NOT NULL,
+  id_carrinho int(11) NOT NULL,
+  id_produto int(11) NOT NULL,
+  quantidade int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices de tabela `tb_categorias`
---
-ALTER TABLE `tb_categorias`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE tb_enderecos (
+  id int(11) NOT NULL,
+  id_usuario int(11) NOT NULL,
+  nome varchar(40) NOT NULL,
+  cep int(11) NOT NULL,
+  rua varchar(80) NOT NULL,
+  numero varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices de tabela `tb_produtos`
---
-ALTER TABLE `tb_produtos`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE tb_produtos (
+  id int(11) NOT NULL,
+  id_categoria int(11) NOT NULL,
+  nome varchar(40) NOT NULL,
+  preco float NOT NULL,
+  imagem text NOT NULL,
+  descricao text NOT NULL,
+  quantidade int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
+CREATE TABLE tb_usuarios (
+  id int(11) NOT NULL,
+  nome varchar(40) NOT NULL,
+  email varchar(40) NOT NULL,
+  senha text NOT NULL,
+  foto varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- AUTO_INCREMENT de tabela `tb_categorias`
---
-ALTER TABLE `tb_categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT de tabela `tb_produtos`
---
-ALTER TABLE `tb_produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE tb_carrinhos
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE tb_categorias
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE tb_compras
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE tb_enderecos
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE tb_produtos
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE tb_usuarios
+  ADD PRIMARY KEY (id);
+
+
+ALTER TABLE tb_carrinhos
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE tb_categorias
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE tb_compras
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE tb_enderecos
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE tb_produtos
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE tb_usuarios
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
